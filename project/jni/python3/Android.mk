f708fe930bb571c124b72080590068a15d23c148
Gerhard Stein <stein@leat.rub.de>
2017-01-28 13:06:28 +0100

    python3 support for x86 arch added. There are still bugs though. With arms it works fine

--- project/jni/python3/Android.mk
+++ project/jni/python3/Android.mk
@@ -3,42 +3,39 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_ARCH),arm)
+$(info Serial result = $(TARGET_ARCH))
 
 LOCAL_MODULE := python3.5m
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/$(TARGET_ARCH)
 
 LOCAL_STATIC_LIBRARIES := 
 
 LOCAL_SHARED_LIBRARIES :=
 
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib/$(TARGET_ARCH) -llog
 
-LOCAL_SRC_FILES = lib/libpython3.5m.so 
+LOCAL_SRC_FILES = lib/$(TARGET_ARCH)/libpython3.5m.so 
 
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_ARCH),arm)
 
 LOCAL_MODULE := python3
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include/$(TARGET_ARCH)
 
 LOCAL_STATIC_LIBRARIES := 
 
 LOCAL_SHARED_LIBRARIES := python3.5m
 
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib/$(TARGET_ARCH) -llog
 
-LOCAL_SRC_FILES = lib/libpython3.so 
+LOCAL_SRC_FILES = lib/$(TARGET_ARCH)/libpython3.so 
 
 include $(PREBUILT_SHARED_LIBRARY)
 
-endif # $(TARGET_ARCH),arm
 
-endif # $(TARGET_ARCH),arm
 
--- project/jni/python3/compile.sh
+++ project/jni/python3/compile.sh
@@ -2,18 +2,22 @@
 
 #ARCH_LIST="arm64-v8a x86 mips armeabi-v7a armeabi"
 
-#ARCH_LIST="arm x86 mips"
+#ARCH_LIST="arm x86 mips arm64"
 ARCH_LIST="arm"
 
 mkdir -p build
 
 build() {
-	export 	ANDROID_PLATFORM=$1
-
+		
 	pushd python3-android
-	make
+	rm env
+        rm mk/env.mk
+	make all ANDROID_PLATFORM=$1
 	popd
 }
+
+
+
 
 PIDS=""
 for ARCH in $ARCH_LIST; do
@@ -25,9 +29,21 @@ for PID in $PIDS; do
 	wait $PID || exit 1
 done
 
-# Provide includes for the to be built apps
-rm include
-rm lib
-ln -s python3-android/build/13b-23-arm-linux-androideabi-4.9/include include 
-ln -s python3-android/build/13b-23-arm-linux-androideabi-4.9/lib lib
-#cp -r -L build/armeabi-v7a/include ./ || exit 1
+
+# Set some symbolic links for android ndk toolkit access
+rm -rf include 
+rm -rf lib
+
+mkdir -p include
+mkdir -p lib
+
+pushd include
+ln -s ../python3-android/build/13b-23-arm-linux-androideabi-4.9/include arm	
+ln -s ../python3-android/build/13b-23-x86-4.9/include x86	
+popd
+
+pushd lib 
+ln -s ../python3-android/build/13b-23-arm-linux-androideabi-4.9/lib arm
+ln -s ../python3-android/build/13b-23-x86-4.9/lib x86
+popd
+
