bf211679e1b569a96ee5c8a44a4638865b6408e9
lubomyr <lubomyr31@gmail.com>
2017-03-20 00:01:22 +0200

    openssl updated to 1.0.2k, implemented x86_64 platform support

--- project/jni/openssl/Android.mk
+++ project/jni/openssl/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_FILENAME := lib$(notdir $(LOCAL_PATH)).so.sdl.1 # It clashes with s
 
 
 ifneq (openssl,$(LOCAL_MODULE))
-ifneq ($(filter arm mips x86 arm64, $(TARGET_ARCH)),)
+ifneq ($(filter arm mips x86 arm64 x86_64, $(TARGET_ARCH)),)
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_SRC_FILES := lib-$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so.sdl.1.so
