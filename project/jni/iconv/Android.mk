d89960517c9a0942b81a2246f71ca35018238108
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-06-21 17:48:55 +0300

    SDL: updated libiconv and libicu, removed prebuilt binaries, now they should be built on the fly

--- project/jni/iconv/Android.mk
+++ project/jni/iconv/Android.mk
@@ -2,20 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifneq ($(TARGET_ARCH_ABI),armeabi-v7a-hard) # Pre-compiled lib disabled for armhf ABI
-ifneq ($(TARGET_ARCH_ABI),arm64-v8a) # Not supported yet
-ifneq ($(TARGET_ARCH_ABI),x86_64) # Not supported yet
-ifneq ($(TARGET_ARCH_ABI),mips64) # Not supported yet
-
 LOCAL_MODULE := $(notdir $(LOCAL_PATH))
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(NDK_TOOLCHAIN_VERSION)/include
 
-LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/$(NDK_TOOLCHAIN_VERSION)/lib$(LOCAL_MODULE).so
 
 include $(PREBUILT_SHARED_LIBRARY)
 
-endif
-endif
-endif
-endif
--- project/jni/icuuc/Android.mk
+++ project/jni/icuuc/Android.mk
@@ -2,11 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifneq ($(TARGET_ARCH_ABI),armeabi-v7a-hard) # Pre-compiled lib disabled for armhf ABI
-ifneq ($(TARGET_ARCH_ABI),arm64-v8a) # Not supported yet
-ifneq ($(TARGET_ARCH_ABI),x86_64) # Not supported yet
-ifneq ($(TARGET_ARCH_ABI),mips64) # Not supported yet
-
 LOCAL_MODULE := $(notdir $(LOCAL_PATH))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
@@ -18,8 +13,3 @@ obj/local/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).a: $(LOCAL_PATH)/$(LOCAL_SRC_FIL
 	cp -f $< $@
 
 include $(PREBUILT_STATIC_LIBRARY)
-
-endif
-endif
-endif
-endif
