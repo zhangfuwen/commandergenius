e618b3a2d0940292cc117bb4495427de828c1020
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-11-14 22:39:14 +0200

    Boost: removed precompiled libraries, updated submodule. Use 'make -f Makefile.prebuilt' from project/jni to generate precompiled libraries, you will need NDK r16-beta2.

--- project/jni/boost/Android.mk
+++ project/jni/boost/Android.mk
@@ -1,19 +1,14 @@
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:=$(call my-dir)
 
 include $(CLEAR_VARS)
 
-ifeq ($(TARGET_ARCH),arm)
-ifeq ($(filter armeabi-v7a-hard, $(APP_ABI)),) # Pre-compiled lib disabled for armhf ABI
-
 LOCAL_MODULE := $(notdir $(LOCAL_PATH))
-ifneq ($(LOCAL_MODULE),boost)
+
+ifneq (boost,$(LOCAL_MODULE))
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).a
 
-LOCAL_SRC_FILES := lib/arm-linux-androideabi-4.6/lib$(LOCAL_MODULE).a
 include $(PREBUILT_STATIC_LIBRARY)
 
 endif
-endif
-
-endif # $(TARGET_ARCH),arm
