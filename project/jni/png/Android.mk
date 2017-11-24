e15c2ff3d693d12130443ad5f2904d9670fed4e5
Sergii Pylypenko <x.pelya.x@gmail.com>
2017-11-24 15:51:10 +0200

    Updated libpng

--- project/jni/png/Android.mk
+++ project/jni/png/Android.mk
@@ -2,26 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := \
-	png.c \
-	pngerror.c \
-	pnggccrd.c \
-	pngget.c \
-	pngmem.c \
-	pngpread.c \
-	pngread.c \
-	pngrio.c \
-	pngrtran.c \
-	pngrutil.c \
-	pngset.c \
-	pngtrans.c \
-	pngvcrd.c \
-	pngwio.c \
-	pngwrite.c \
-	pngwtran.c \
-	pngwutil.c
+LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
 
-LOCAL_CFLAGS := -std=gnu89 -fvisibility=hidden ## -fomit-frame-pointer -ftrapv
+LOCAL_CFLAGS := -DHAVE_CONFIG_H
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 # LOCAL_LDLIBS := z
 
