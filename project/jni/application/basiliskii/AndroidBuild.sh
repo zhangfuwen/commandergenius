dcad4c156cbda72048d064875cb60408192db504
lubomyr <lubomyr31@gmail.com>
2018-01-13 19:30:28 +0200

    Basilisk II: Updated to version 1.0.0.05. Updated app icon, configuration, assets, and build script

--- project/jni/application/basiliskii/AndroidBuild.sh
+++ project/jni/application/basiliskii/AndroidBuild.sh
@@ -9,6 +9,7 @@ ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/$1/libpthread.so
 ln -sf libsdl_image.so $LOCAL_PATH/../../../obj/local/$1/libSDL_image.so
 ln -sf libsdl_ttf.so $LOCAL_PATH/../../../obj/local/$1/libSDL_ttf.so
 
+JOBS=4
 
 if [ \! -f basiliskii/src/Unix/configure ] ; then
 	sh -c "cd basiliskii/src/Unix && ./autogen.sh"
@@ -20,5 +21,5 @@ if [ \! -f basiliskii/src/Unix/Makefile ] ; then
 	../setEnvironment-$1.sh sh -c "cd basiliskii/src/Unix && ./configure --build=x86_64-unknown-linux-gnu --host=$2 --enable-sdl-video --enable-sdl-audio --without-gtk --without-esd --without-x"
 fi
 
-make -C basiliskii/src/Unix && mv -f basiliskii/src/Unix/BasiliskII libapplication-$1.so
+make -C basiliskii/src/Unix -j$JOBS && mv -f basiliskii/src/Unix/BasiliskII libapplication-$1.so
 
