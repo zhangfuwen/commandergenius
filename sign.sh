49c47093c011ea1c7f0b805eeab0ec77f0925806
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-01-29 17:53:55 +0200

    Updated sign scripts to use apksigner instead of jarsigner

--- sign.sh
+++ sign.sh
@@ -11,40 +11,13 @@ cd project/app/build/outputs/apk/
 # Remove old certificate
 rm -f Signed.apk
 cp -f app-release.apk Signed.apk
-zip -d Signed.apk "META-INF/*"
+#zip -d Signed.apk "META-INF/*"
 # Sign with the new certificate
+rm -f ../../../../../$APPNAME-$APPVER.apk
+zipalign 4 Signed.apk ../../../../../$APPNAME-$APPVER.apk
+rm -f Signed.apk
 echo Using keystore $ANDROID_KEYSTORE_FILE and alias $ANDROID_KEYSTORE_ALIAS
 stty -echo
-jarsigner -verbose -tsa http://timestamp.digicert.com -keystore $ANDROID_KEYSTORE_FILE -sigalg MD5withRSA -digestalg SHA1 Signed.apk $ANDROID_KEYSTORE_ALIAS || exit 1
+apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS ../../../../../$APPNAME-$APPVER.apk || exit 1
 stty echo
 echo
-rm -f app-release.apk
-zipalign 4 Signed.apk app-release.apk
-rm -f Signed.apk
-cp -f app-release.apk ../../../../../$APPNAME-$APPVER.apk
-
-if false; then
-#DEBUGINFODIR=`aapt dump badging App.apk | grep "package:" | sed "s/.*name=[']\([^']*\)['].*versionCode=[']\([^']*\)['].*/\1-\2/" | tr " '/" '---'`
-DEBUGINFODIR=$APPNAME-$APPVER
-echo Copying debug info to project/debuginfo/$DEBUGINFODIR
-mkdir -p ../debuginfo/$DEBUGINFODIR/x86 ../debuginfo/$DEBUGINFODIR/armeabi-v7a
-cp -f ../obj/local/x86/*.so ../debuginfo/$DEBUGINFODIR/x86
-cp -f ../obj/local/armeabi-v7a/*.so ../debuginfo/$DEBUGINFODIR/armeabi-v7a
-cp -f app-release.apk ../debuginfo/$DEBUGINFODIR/$APPNAME-$APPVER.apk
-fi
-
-if false; then
-if [ -n "$ANDROID_UPLOAD_KEYSTORE_FILE" ]; then
-cd ../../../../../
-cp -f $APPNAME-$APPVER.apk $APPNAME-$APPVER-upload1.apk
-# Sign with the upload certificate
-echo Using keystore $ANDROID_UPLOAD_KEYSTORE_FILE and alias $ANDROID_UPLOAD_KEYSTORE_ALIAS
-stty -echo
-jarsigner -verbose -tsa http://timestamp.digicert.com -keystore $ANDROID_UPLOAD_KEYSTORE_FILE -sigalg MD5withRSA -digestalg SHA1 $APPNAME-$APPVER-upload1.apk $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
-stty echo
-echo
-rm -f $APPNAME-$APPVER-upload.apk
-zipalign 4 $APPNAME-$APPVER-upload1.apk $APPNAME-$APPVER-upload.apk
-rm -f $APPNAME-$APPVER-upload1.apk
-fi
-fi
