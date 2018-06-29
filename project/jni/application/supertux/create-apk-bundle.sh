fc7e6e63b282e47088bc44a3cdcea53d8ab3880a
Sergii Pylypenko <x.pelya.x@gmail.com>
2018-06-29 23:32:13 +0300

    SuperTux: The Final Update

--- project/jni/application/supertux/create-apk-bundle.sh
+++ project/jni/application/supertux/create-apk-bundle.sh
@@ -6,5 +6,6 @@ cp ../../../../project/app/build/outputs/apk/app-release.apk $OUT || exit 1
 cd supertux/data || exit 1
 zip -r -9 $OUT * || exit 1
 zipalign 4 $OUT $OUT-aligned || exit 1
-apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --ks-pass pass:android $OUT-aligned || exit 1
+#apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --ks-pass pass:android $OUT-aligned || exit 1
+apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS $OUT-aligned || exit 1
 mv $OUT-aligned $OUT
