8c34faa475f004c6fd6152dd306cbe255a51e8fc
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-04-26 22:54:54 +0300

    OpenTTD: updated build scripts

--- project/jni/application/openttd/AndroidBuild.sh
+++ project/jni/application/openttd/AndroidBuild.sh
@@ -17,21 +17,24 @@ CPU_TYPE=32
 	cp -a src/bin/baseset openttd-$VER-$1/bin/
 } || exit 1
 
-[ -e openttd-$VER-$1/objs/lang/english.lng ] || {
-	sh -c "cd openttd-$VER-$1 && ../src/configure --without-freetype --without-png --without-zlib \
-			--without-lzo2 --without-lzma --cpu-type=$CPU_TYPE && \
-			make lang && make -C objs/release endian_target.h depend && make -C objs/setting" || exit 1
-	rm -f openttd-$VER-$1/Makefile
-} || exit 1
+#[ -e openttd-$VER-$1/objs/lang/english.lng ] || {
+#	sh -c "cd openttd-$VER-$1 && ../src/configure --without-freetype --without-png --without-zlib \
+#			--without-lzo2 --without-lzma --cpu-type=$CPU_TYPE && \
+#			make lang && make -C objs/release endian_target.h depend && make -C objs/setting" || exit 1
+#	rm -f openttd-$VER-$1/Makefile
+#} || exit 1
 
 [ -e openttd-$VER-$1/Makefile ] || {
 	rm -f src/src/rev.cpp
 	env PATH=$LOCAL_PATH/..:$PATH \
 	env LDFLAGS=-L$LOCAL_PATH/../../../obj/local/$ARCH \
+	env CFLAGS_BUILD="-I." \
+	env CXXFLAGS_BUILD="-I." \
+	env LDFLAGS_BUILD="-L." \
 	env CLANG=1 ../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && env ../src/configure \
 		--with-sdl --with-freetype --with-png --with-zlib --with-icu --with-libtimidity='pkg-config libtimidity' \
 		--with-lzo2=$LOCAL_PATH/../../../obj/local/$ARCH/liblzo2.so --prefix-dir='.' --data-dir='' \
-		--without-allegro --with-fontconfig --with-lzma --cpu-type=$CPU_TYPE --os=android"
+		--without-allegro --with-fontconfig --with-lzma --cpu-type=$CPU_TYPE --os=android --cc-build=gcc --cxx-build=g++"
 } || exit 1
 
 NCPU=4
