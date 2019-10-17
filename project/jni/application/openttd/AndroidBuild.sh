32442641dc8df4472f4e77caa04ab7c8b2e2f6ca
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-10-17 21:19:43 +0300

    OpenTTD: updated build script

--- project/jni/application/openttd/AndroidBuild.sh
+++ project/jni/application/openttd/AndroidBuild.sh
@@ -32,7 +32,7 @@ CPU_TYPE=32
 	env CXXFLAGS_BUILD="-I." \
 	env LDFLAGS_BUILD="-L." \
 	env CLANG=1 ../setEnvironment-$1.sh sh -c "cd openttd-$VER-$1 && env ../src/configure \
-		--with-sdl --with-freetype --with-png --with-zlib --with-icu --with-libtimidity --without-fluidsynth \
+		--with-sdl=sdl1 --with-freetype --with-png --with-zlib --with-icu --with-libtimidity --without-fluidsynth \
 		--with-lzo2=$LOCAL_PATH/../../../obj/local/$ARCH/liblzo2.so --prefix-dir='.' --data-dir='' \
 		--without-allegro --with-fontconfig --with-lzma --cpu-type=$CPU_TYPE --os=android --cc-build=gcc --cxx-build=g++"
 } || exit 1
