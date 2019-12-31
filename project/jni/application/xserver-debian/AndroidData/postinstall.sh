30ca76a91b3e7555e9a2318dd01ebe4ec21da954
Sergii Pylypenko <x.pelya.x@gmail.com>
2019-12-31 22:00:57 +0200

    Debian: updated postinstall script

--- project/jni/application/xserver-debian/AndroidData/postinstall.sh
+++ project/jni/application/xserver-debian/AndroidData/postinstall.sh
@@ -6,10 +6,10 @@ OBB_VERSION=$ANDROID_PACKAGE_VERSION_CODE
 echo "Extracting data files"
 cd $SECURE_STORAGE_DIR
 echo "./busybox tar xvJf $ANDROID_OBB_DIR/main.$OBB_VERSION.$ANDROID_PACKAGE_NAME.obb"
-./busybox tar xvJf $ANDROID_OBB_DIR/main.$OBB_VERSION.$ANDROID_PACKAGE_NAME.obb || { echo "Extracting data failed!" ; exit 1 ; }
+./busybox tar xvJf $ANDROID_OBB_DIR/main.$OBB_VERSION.$ANDROID_PACKAGE_NAME.obb
 echo "Extracting overlay data files"
 echo "./busybox tar xvJf $DATADIR/overlay.tar.xz"
-./busybox tar xvJf $DATADIR/overlay.tar.xz || { echo "Extracting overlay data failed!" ; exit 1 ; }
+./busybox tar xvJf $DATADIR/overlay.tar.xz
 cd $SECURE_STORAGE_DIR/img
 
 echo "Installation path: $SECURE_STORAGE_DIR/img"
